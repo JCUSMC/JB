2801fd5a831ed52be4f021dedea5c70bae8e60c3
JCUSMC <joshua.harwood@gmail.com>
2018-11-13 23:31:23 -0600

    webscrape practice

---
+++ ScrapePract.R
@@ -0,0 +1,39 @@
+#Loading the rvest package
+install.packages('rvest')
+library('rvest')
+
+#Specifying the url for desired website to be scrapped
+url <- 'http://www.imdb.com/search/title?count=100&release_date=2016,2016&title_type=feature'
+
+#Reading the HTML code from the website
+webpage <- read_html(url)
+
+# Now, we’ll be scraping the following data from this website.
+# 
+# Rank: The rank of the film from 1 to 100 on the list of 100 most popular feature films released in 2016.
+# Title: The title of the feature film.
+# Description: The description of the feature film.
+# Runtime: The duration of the feature film.
+# Genre: The genre of the feature film,
+# Rating: The IMDb rating of the feature film.
+# Metascore: The metascore on IMDb website for the feature film.
+# Votes: Votes cast in favor of the feature film.
+# Gross_Earning_in_Mil: The gross earnings of the feature film in millions.
+# Director: The main director of the feature film. Note, in case of multiple directors, I’ll take only the first.
+# Actor: The main actor of the feature film. Note, in case of multiple actors, I’ll take only the first.
+
+#Using CSS selectors to scrap the rankings section
+rank_data_html <- html_nodes(webpage,'.text-primary')
+
+#Converting the ranking data to text
+rank_data <- html_text(rank_data_html)
+
+#Let's have a look at the rankings
+head(rank_data)
+
+#Data-Preprocessing: Converting rankings to numerical
+rank_data<-as.numeric(rank_data)
+
+#Let's have another look at the rankings
+head(rank_data)
+
