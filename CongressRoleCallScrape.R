8f284b58206ffad1e4805b34b36c95541b969b0f
JCUSMC <joshua.harwood@gmail.com>
2018-11-25 12:26:41 -0600

    update

--- CongressRoleCallScrape.R
+++ CongressRoleCallScrape.R
@@ -3,7 +3,7 @@ install.packages('XML')
 library('XML')
 
 #Specifying the url for desired website to be scrapped
-url <- "http://clerk.house.gov/evs/2017/roll198.xml"
+url <- "http://clerk.house.gov/evs/2018/roll398.xml"
 xmldoc <- xmlParse(url)
 rootNode <- xmlRoot(xmldoc)
 rootNode[1]
@@ -20,9 +20,6 @@ rollcall = xmlSApply(rootNode[[1]][["rollcall-num"]], xmlValue)
 
 
 
-vote_data = xmlSApply(rootNode[[2]][["recorded-vote"]], xmlValue)
-vote_data
-
 # Read and transpose all of the "recorded-vote" child node values
 all = t(xmlSApply(rootNode[[2]], function(x) xmlSApply(x,xmlValue)))
 # Convert Columns to data frame
@@ -30,8 +27,8 @@ leg_vote1 = as.data.frame(all[,1], row.names = NULL)
 leg_vote2 = as.data.frame(all[,2], row.names = NULL)
 unique(leg_vote2)
 # Aye = 1, No = 0, Not Voting = 0
-leg_vote3 = ifelse(leg_vote2 == "Aye",1,0)
-
+leg_vote3 = ifelse(leg_vote2  == "Aye"| leg_vote2 == "Yea",1,0)
+# leg_vote2
 
 # create dataset and assign column names
 leg_vote = cbind(congress,session,rollcall,legislator=leg_vote1,vote=leg_vote3)
