42d26efcfe581201ab8778e9ea161f4399f6aeee
JCUSMC <joshua.harwood@gmail.com>
2018-11-25 12:09:44 -0600

    update

--- CongressRoleCallScrape.R
+++ CongressRoleCallScrape.R
@@ -23,12 +23,19 @@ rollcall = xmlSApply(rootNode[[1]][["rollcall-num"]], xmlValue)
 vote_data = xmlSApply(rootNode[[2]][["recorded-vote"]], xmlValue)
 vote_data
 
-# Read and transpose all of the "recorded-vote" child node
+# Read and transpose all of the "recorded-vote" child node values
 all = t(xmlSApply(rootNode[[2]], function(x) xmlSApply(x,xmlValue)))
+# Convert Columns to data frame
 leg_vote1 = as.data.frame(all[,1], row.names = NULL)
 leg_vote2 = as.data.frame(all[,2], row.names = NULL)
+unique(leg_vote2)
+# Aye = 1, No = 0, Not Voting = 0
+leg_vote3 = ifelse(leg_vote2 == "Aye",1,0)
+
+
+# create dataset and assign column names
 leg_vote = cbind(congress,session,rollcall,legislator=leg_vote1,vote=leg_vote2)
 colnames(leg_vote) = c('congress','session','rollcall','legislator','vote')
 leg_vote
-
+unique(leg_vote[,5])
 
